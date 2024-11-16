d24f3821ba660cb275a4f1a1b50c6520e127d0cc
Guilherme Ferreira <guig_ferreira@outlook.com>
2024-11-16 16:23:31 -0300

    Segundo Commit

--- src/routes/RoutesConfig.jsx
+++ src/routes/RoutesConfig.jsx
@@ -1,20 +1,21 @@
 import React from 'react';
-import { Routes, Route } from 'react-router-dom';
 import Home from '../pages/Home';
-import About from '../pages/About';
 import SignUp from '../pages/SignUp';
 import GamesPage from '../pages/GamesPage';
 
-function RoutesConfig() {
-    return(
-        <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/games" element={<GamesPage />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/signup" element={<SignUp />} />
-            <Route path="*" element={<h2>Página não encontrada</h2>} />
-        </Routes>
-    )
-}
+import createBrowserRouter from "react-router-dom";
 
-export default RoutesConfig;
+export const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <Home />,
+    },
+    {
+        path: "/games",
+        element: <GamesPage />,
+    },
+    {
+        path: "/signup",
+        element: <SignUp />,
+    },
+]);
